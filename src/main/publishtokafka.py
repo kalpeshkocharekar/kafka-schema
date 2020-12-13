9fbafb7eb3482ee1033f327b89178d923356d1dd
Kalpesh Kocharekar <kalpeshk71@gmail.com>
2020-12-13 22:40:43 +0530

    making topic and server paramterised

--- src/main/publishtokafka.py
+++ src/main/publishtokafka.py
@@ -4,7 +4,10 @@ import time
 from bson import json_util
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+topic = "sample"
+server = "localhost:9092"
+
+producer = KafkaProducer(bootstrap_servers=[server])
 '''data format 1:'''
 data1 = {'tag': 'test',
          'name': 'hashtag'
@@ -17,7 +20,7 @@ data2 = {
 }
 
 while True:
-    producer.send('sample', key=b"data1", value=json.dumps(data1, default=json_util.default).encode('utf-8'))
-    producer.send('sample', key=b'data2', value=json.dumps(data2, default=json_util.default).encode('utf-8'))
+    producer.send(topic, key=b"data1", value=json.dumps(data1, default=json_util.default).encode('utf-8'))
+    producer.send(topic, key=b'data2', value=json.dumps(data2, default=json_util.default).encode('utf-8'))
     producer.flush()
     time.sleep(10)
